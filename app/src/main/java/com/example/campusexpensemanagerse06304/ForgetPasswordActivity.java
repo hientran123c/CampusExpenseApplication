33281661ce42ccce04dc8869929926cb2d4c450c
neck5656 <nightvision12345678901234@gmail.com>
2025-03-24 07:41:17 +0700

    Update code

--- app/src/main/java/com/example/campusexpensemanagerse06304/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/campusexpensemanagerse06304/ForgetPasswordActivity.java
@@ -1,14 +1,58 @@
 package com.example.campusexpensemanagerse06304;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.campusexpensemanagerse06304.database.UserDb;
+
 public class ForgetPasswordActivity extends AppCompatActivity {
+    Button btnConfirm;
+    EditText edtAccount,edtEmail;
+    UserDb userDb;
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+        userDb = new UserDb(ForgetPasswordActivity.this);
+        btnConfirm =findViewById(R.id.btnConfirmAccount);
+        edtAccount =findViewById(R.id.edtAccount);
+        edtEmail =findViewById(R.id.edtEmail);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = edtAccount.getText().toString().trim();
+                if(TextUtils.isEmpty(account)){
+                    edtAccount.setError("Account can not empty");
+                    return;
+                }
+                String email = edtEmail.getText().toString().trim();
+                if (TextUtils.isEmpty(email)){
+                    edtEmail.setError("Email can not empty");
+                    return;
+
+                }
+                boolean checking = userDb.checkExistsUsername(account,email);
+                if(checking){
+                    Intent intent = new Intent(ForgetPasswordActivity.this,UpdatePasswordActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("ACCOUNT_FORGET_PW",account);
+                    bundle.putString("EMAIL_FORGET_PW",email);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+
+                }else {
+                    Toast.makeText(ForgetPasswordActivity.this, "Account Invalid", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/campusexpensemanagerse06304/UpdatePasswordActivity.java
+++ app/src/main/java/com/example/campusexpensemanagerse06304/UpdatePasswordActivity.java
@@ -1,4 +1,66 @@
 package com.example.campusexpensemanagerse06304;
 
-public class UpdatePasswordActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.campusexpensemanagerse06304.database.UserDb;
+
+public class UpdatePasswordActivity extends AppCompatActivity {
+    Button btnSaveChangePw;
+    EditText edtNewPw, edtConfirmPw;
+    UserDb userDb;
+    Intent intent;
+    Bundle bundle;
+    private String account = null;
+    private String email = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_password);
+        btnSaveChangePw = findViewById(R.id.btnSaveChangePassword);
+        edtNewPw = findViewById(R.id.edtNewPassword);
+        edtConfirmPw = findViewById(R.id.edtConfirmNewPassword);
+        userDb  = new UserDb(UpdatePasswordActivity.this);
+        intent= getIntent();
+        bundle = intent.getExtras();
+        if(bundle != null){
+            account = bundle.getString("ACCOUNT_FORGET_PW","");
+            email = bundle.getString("EMAIL_FORGET_PW","");
+        }
+        btnSaveChangePw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newPw = edtNewPw.getText().toString().trim();
+                if (TextUtils.isEmpty(newPw)) {
+                    edtNewPw.setError("New Password can not empty");
+                    return;
+                }
+                String confirmNewPw = edtConfirmPw.getText().toString().trim();
+                if (! confirmNewPw.equals(newPw)) {
+                    edtConfirmPw.setError("Confirm New Password not equal New Password");
+                    return;
+                }
+                int update = userDb.changePassword(newPw, account, email);
+                if (update == -1) {
+                    Toast.makeText(UpdatePasswordActivity.this, "Update Failed", Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    Toast.makeText(UpdatePasswordActivity.this, "Update Success", Toast.LENGTH_SHORT).show();
+                    Intent intentLogin = new Intent(UpdatePasswordActivity.this, SignInActivity.class);
+                    startActivity(intentLogin);
+                    finish();
+                }
+            }
+
+        });
+    }
 }
